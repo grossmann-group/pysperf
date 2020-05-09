36ed871430d0020d098a6ac82a20cfbd995e1e65
Qi Chen <qichen@andrew.cmu.edu>
2020-05-09 17:30:38 -0400

    setting up a matrix runner. need to define model types

--- pysperf/pysperf_main.py
+++ pysperf/pysperf_main.py
@@ -3,10 +3,12 @@ from pyutilib.misc import Container
 
 from model_library_tools import compute_model_stats
 from config import models
+from run_manager import execute_matrix_run
 from solver_library_tools import list_solver_capabilities
 
 if __name__ == "__main__":
     from pysperf.models import *  # Imports the models and registers them
     from pysperf.solvers import *  # Imports the solvers and registers them
     # compute_model_stats()
-    list_solver_capabilities()
+    # list_solver_capabilities()
+    execute_matrix_run()
--- pysperf/run_manager.py
+++ pysperf/run_manager.py
@@ -1,7 +1,17 @@
 
+from pysperf.model_library import models
+from pysperf.solver_library import solvers
+
 
 def execute_matrix_run():
     # create matrix of jobs to run
+    jobs = {
+        (model_name, solver_name)
+        for model_name, model in models.items()
+        for solver_name, solver in solvers.items()
+        if model.model_type in solver.compatible_model_types
+    }
+    print(jobs)
     # create directories and files
     # submit job
     pass
