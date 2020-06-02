e9f4a340f263a0e6ef91d78a843cea04424e39a7
Qi Chen <qichen@andrew.cmu.edu>
2020-06-02 14:06:30 -0400

    add preliminary support for specifying dual bounds and infeasible models. Removing three models that do not compile properly.

--- pysperf/model_library_registration.py
+++ pysperf/model_library_registration.py
@@ -1,15 +1,17 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 
 import pyomo.environ as pyo
 
-from .base_classes import _TestModel
+from .base_classes import _Infeasible, _TestModel
 from .config import models
 
 
 def register_model(
         name: str, build_function: Callable,
         model_type=None, convex=None,
-        best_value=None, opt_value=None, bigM=None) -> None:
+        best_value=None, best_dual=None,
+        opt_value=None,
+        bigM=None) -> None:
     """
     Registers the model in the model library.
 
@@ -19,13 +21,15 @@ def register_model(
         Registry name for the model.
     build_function
         Function that returns a `pyomo.environ.ConcreteModel` object
-    model_type : pyspa.model_types.ModelType, optional
+    model_type : pysperf.model_types.ModelType, optional
         Model classification (e.g. MINLP)
     convex : bool, optional
         Indicate whether the model is convex
     best_value: float, optional
         Objective value of best known solution. Either `best_value` or `opt_value` must be specified.
-    opt_value: float, optional
+    best_dual: float, optional
+        Objective value of best dual solution.
+    opt_value: float or _Infeasible, optional
         Objective value of optimal solution. Either `best_value` or `opt_value` must be specified.
     bigM : float, optional
         Default Big-M parameter value to use.
@@ -40,6 +44,7 @@ def register_model(
     new_model.model_type = model_type
     new_model.convex = convex
     new_model.best_value = best_value
+    new_model.best_dual = best_dual
     new_model.opt_value = opt_value
 
     # Add default BigM if one was offered
@@ -59,14 +64,19 @@ def register_model(
 
 def register_model_builder(
         name: Optional[str] = None, model_type=None, convex: Optional[bool] = None,
-        best_value: Optional[float] = None, opt_value: Optional[float] = None,
+        best_value: Optional[float] = None, best_dual: Optional[float] = None,
+        opt_value: Union[Optional[float], _Infeasible] = None,
         bigM: Optional[float] = None) -> Callable:
     def anon_decorator(build_function):
-        register_model(build_function.__name__, build_function, model_type, convex, best_value, opt_value, bigM)
+        register_model(
+            name=build_function.__name__, build_function=build_function, model_type=model_type,
+            convex=convex, best_value=best_value, best_dual=best_dual, opt_value=opt_value, bigM=bigM)
         return build_function
 
     def named_decorator(build_function):
-        register_model(name, build_function, model_type, convex, best_value, opt_value, bigM)
+        register_model(
+            name=name, build_function=build_function, model_type=model_type,
+            convex=convex, best_value=best_value, best_dual=best_dual, opt_value=opt_value, bigM=bigM)
         return build_function
 
     if name is None:
--- pysperf/models/minlplib.py
+++ pysperf/models/minlplib.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 from pathlib import Path
 
 import pandas
 from pyutilib.misc import import_file
 
+from pysperf.base_classes import InfeasibleExpected
 from pysperf.model_library_registration import register_model
 
 minlplibdir = Path(__file__).parent.joinpath("minlplib/")
@@ -12,21 +14,22 @@ with minlplibdir.joinpath("MINLP.solu").open() as solufile:
     pd = pandas.read_csv(solufile, sep=r'\s+', header=None)
     pd.columns = ['soln_type', 'model', 'value']
 
-    model_solution_data = {}
+    model_solution_data = defaultdict(dict)
     for row in pd.itertuples(index=False):
         soln_type = row.soln_type
         model = row.model
         value = row.value
         if soln_type == "=opt=":
-            model_solution_data[model] = {'opt_value': value}
+            model_solution_data[model]['opt_value'] = value
         elif soln_type == "=best=":
-            model_solution_data[model] = {'best_value': value}
+            model_solution_data[model]['best_value'] = value
         elif soln_type == "=bestdual=":
-            pass  # We do not yet have handling for best lower bound
+            model_solution_data[model]['best_dual'] = value
         elif soln_type == "=inf=":
-            pass  # We do not yet have handling for infeasibility
+            model_solution_data[model]['opt_value'] = InfeasibleExpected
         else:
             raise NotImplementedError(f"Unrecognized solu file solution type: '{soln_type}'")
+    model_solution_data = dict(model_solution_data)
 
 
 def _build_from_file_import(model_file_path: Path):
@@ -40,17 +43,19 @@ def _build_from_file_import(model_file_path: Path):
     return model_constructor
 
 
-# for modelfile in minlplibdir.glob("*.py"):
-#     try:
-#         register_model(
-#             name=modelfile.stem,
-#             build_function=_build_from_file_import(modelfile),
-#             opt_value=model_solution_data[modelfile.stem].get('opt_value', None),
-#             best_value=model_solution_data[modelfile.stem].get('best_value', None)
-#         )
-#     except KeyError as err:
-#         if str(err) == f"'{modelfile.stem}'":
-#             print(f"Model {modelfile.stem} missing solution information. Omitting from library.")
-#             continue
-#         else:
-#             raise
+for modelfile in minlplibdir.glob("*.py"):
+    try:
+        model_info = model_solution_data[modelfile.stem]
+        register_model(
+            name=modelfile.stem,
+            build_function=_build_from_file_import(modelfile),
+            opt_value=model_info.get('opt_value', None),
+            best_value=model_info.get('best_value', None),
+            best_dual=model_info.get('best_dual', None),
+        )
+    except KeyError as err:
+        if str(err) == f"'{modelfile.stem}'":
+            print(f"Model {modelfile.stem} missing solution information. Omitting from library.")
+            continue
+        else:
+            raise
